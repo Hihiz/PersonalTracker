e0c5ad8b6a934d0621e93f0e82f4c780ca0a35b8
Hihiz <public@mail.ru>
2023-06-17 00:31:53 +0300

    Свойство для отображения иконки и названия

--- PersonalTracker/Models/Category.cs
+++ PersonalTracker/Models/Category.cs
@@ -14,5 +14,14 @@ namespace PersonalTracker.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
