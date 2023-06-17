e9426937cf612a86769d4031315b477e4b23eeff
Hihiz <public@mail.ru>
2023-06-18 01:39:31 +0300

    Изменить методы действия Create и Edit в контроллере Transaction

--- PersonalTracker/Controllers/TransactionController.cs
+++ PersonalTracker/Controllers/TransactionController.cs
@@ -36,90 +36,43 @@ namespace PersonalTracker.Controllers
             return View(transaction);
         }
 
-        public IActionResult Create()
+        public IActionResult AddEdit(int id = 0)
         {
-            ViewData["CategoryId"] = new SelectList(_db.Categories, "Id", "Title");
+            //ViewData["CategoryId"] = new SelectList(_db.Categories, "Id", "Title");
 
-            return View();
+            PopulateCategories();
+
+            if (id == 0)
+            {
+                return View(new Transaction());
+            }
+            else
+            {
+                return View(_db.Transactions.Find(id));
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,CategoryId,Amount,Note,Date")] Transaction transaction)
+        public async Task<IActionResult> AddEdit([Bind("Id,CategoryId,Amount,Note,Date")] Transaction transaction)
         {
             if (ModelState.IsValid)
             {
-                _db.Add(transaction);
+                if (transaction.Id == 0)
+                {
+                    _db.Add(transaction);
+                }
+                else
+                {
+                    _db.Update(transaction);
+                }
+
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewData["CategoryId"] = new SelectList(_db.Categories, "Id", "Title", transaction.CategoryId);
-
-            return View(transaction);
-        }
-
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _db.Transactions == null)
-            {
-                return NotFound();
-            }
-
-            Transaction transaction = await _db.Transactions.FindAsync(id);
-
-            if (transaction == null)
-            {
-                return NotFound();
-            }
-
-            ViewData["CategoryId"] = new SelectList(_db.Categories, "Id", "Title", transaction.CategoryId);
-
-            return View(transaction);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,CategoryId,Amount,Note,Date")] Transaction transaction)
-        {
-            if (id != transaction.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _db.Update(transaction);
-                    await _db.SaveChangesAsync();
-                }
-                catch (Exception ex)
-                {
-                    return NotFound();
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            ViewData["CategoryId"] = new SelectList(_db.Categories, "Id", "Title", transaction.CategoryId);
-
-            return View(transaction);
-        }
-
-        public async Task<IActionResult> Delete(int id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Transaction transaction = await _db.Transactions.Include(t => t.Category).FirstOrDefaultAsync(t => t.Id == id);
-
-            if (transaction == null)
-            {
-                return NotFound();
-            }
+            //ViewData["CategoryId"] = new SelectList(_db.Categories, "Id", "Title", transaction.CategoryId);
+            PopulateCategories();
 
             return View(transaction);
         }
@@ -142,6 +95,16 @@ namespace PersonalTracker.Controllers
 
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var categoryList = _db.Categories.ToList();
+            Category defaultCategory = new Category() { Id = 0, Title = "Выберите категорию" };
+            categoryList.Insert(0, defaultCategory);
+
+            ViewBag.Categories = categoryList;
         }
     }
 }
